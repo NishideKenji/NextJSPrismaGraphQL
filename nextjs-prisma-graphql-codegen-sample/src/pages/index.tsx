018229682228fbdc734b05bb7b1627cf1af5a959
NishideKenji <132211570+NishideKenji@users.noreply.github.com>
2023-08-13 23:50:44 +0900

    ログイン機能追加

--- nextjs-prisma-graphql-codegen-sample/src/pages/index.tsx
+++ nextjs-prisma-graphql-codegen-sample/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
+import { signIn } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <main className="p-10">test</main>
+  return (
+    <main className="p-10">
+      {process.env.NODE_ENV === 'development' && (
+        <button onClick={() => signIn()}>Login</button>
+      )}
+    </main>
+  )
 }
